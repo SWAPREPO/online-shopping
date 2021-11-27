34c47dd47e9c688bdde993913d3bd88de4931b8a
swapnil.thoke <sst231990@gmail.com>
2021-11-27 15:52:15 +0530

    Commit 4.

--- onlineshopping/src/main/java/com/sst/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/sst/onlineshopping/controller/PageController.java
@@ -11,15 +11,7 @@ public class PageController {
 	public ModelAndView index()
 	{
 		ModelAndView mv =  new ModelAndView("page");
-<<<<<<< HEAD
-<<<<<<< HEAD
-		mv.addObject("greeting", "Welcome to spring MVC");
-=======
-		mv.addObject("greeting", "Welcome to my spring MVC");
->>>>>>> refs/remotes/origin/main
-=======
 		mv.addObject("greeting", "Welcome to swapnil my spring MVC");
->>>>>>> refs/remotes/origin/main
 		return mv;
 	}
 	
