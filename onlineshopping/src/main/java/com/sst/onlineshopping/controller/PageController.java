e24744e79b39f608a602098ba42fb9dfe1f8c421
SWAPREPO <90626445+SWAPREPO@users.noreply.github.com>
2021-11-27 14:50:34 +0530

    Git fifth updated.

--- onlineshopping/src/main/java/com/sst/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/sst/onlineshopping/controller/PageController.java
@@ -11,7 +11,7 @@ public class PageController {
 	public ModelAndView index()
 	{
 		ModelAndView mv =  new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to my spring MVC");
+		mv.addObject("greeting", "Welcome to swapnil my spring MVC");
 		return mv;
 	}
 	
