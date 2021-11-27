c47417fd9799e7af3d1b864e9a20a5febedb3869
SWAPREPO <90626445+SWAPREPO@users.noreply.github.com>
2021-11-27 14:32:28 +0530

    Update on repositry url

--- onlineshopping/src/main/java/com/sst/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/sst/onlineshopping/controller/PageController.java
@@ -11,7 +11,7 @@ public class PageController {
 	public ModelAndView index()
 	{
 		ModelAndView mv =  new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to spring MVC");
+		mv.addObject("greeting", "Welcome toddd spring MVC");
 		return mv;
 	}
 	
